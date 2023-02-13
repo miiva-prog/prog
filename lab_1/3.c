47db727a255ec2b91d6fd547ea1fb916a5d2cf82
root <root@LAPTOP-6QUNCNHT>
2023-02-13 10:23:46 +0700

    lab_1

--- lab_1/3.c
+++ lab_1/3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#define SQR(x) x * x
+#define SQR(x) ((x) * (x))
 
 int main()
 {
-    int y = 5,x = y + 1;
-    int z = SQR(x);
+    int y = 5;
+    int z = SQR(y + 1);
 
     printf("z = %d\n",z);
 
